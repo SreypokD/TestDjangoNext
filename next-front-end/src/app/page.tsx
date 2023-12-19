3f482094f8fa6f395c82290b81d83e024f01e4d9
SreypokD <you@example.com>
2023-12-19 23:03:39 +0700

    change style

--- next-front-end/src/app/page.tsx
+++ next-front-end/src/app/page.tsx
@@ -1,9 +1,11 @@
-import Navigation from "./Dashboard/navigation/navbar"
+import Navigation from "../components/navigation/navbar"
+// import NavLinks from "@/components/menu/navlink"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
+    <main className="flex flex-col items-center justify-between ">
         <Navigation/>
+        <h1 className="font-bold text-2xl mt-4">TODO LIST</h1>
     </main>
   )
 }
